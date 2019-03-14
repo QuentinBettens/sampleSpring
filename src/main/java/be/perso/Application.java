769e15f927a804b3a417ccfcbb6642d5a1519370
bttqnt <quentin.bettens@forem.be>
2019-03-14 22:50:40 +0100

    # delete unused import

--- src/main/java/be/perso/Application.java
+++ src/main/java/be/perso/Application.java
@@ -5,10 +5,8 @@ import be.perso.service.CarService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-
 public class Application {
     public static void main(String[] args) {
-
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
         CarService carService = applicationContext.getBean("carService", CarService.class);
         Car car = carService.findAll().get(0);
